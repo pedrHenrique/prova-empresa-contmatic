552d9de9278d69fd3882f9f74b685555bd99fbd0
pedrHenrique <pedrleonardi@gmail.com>
2020-07-26 23:25:16 -0300

    Teste

---
+++ workspaceEmpresa-03.7.9/projeto-empresav1/src/test/java/br/com/contmatic/empresav1/ITtest/DepartamentoIT.java
@@ -0,0 +1,11 @@
+package br.com.contmatic.empresav1.ITtest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({})
+public class DepartamentoIT {
+
+}
